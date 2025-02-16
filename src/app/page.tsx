0c96a4c567961356d2730c2933a4495b5569a3b4
ArthurEstevaum <arthurestevao631@gmail.com>
2025-02-16 12:51:00 -0300

    Finish main section markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,101 @@
 import Image from "next/image";
+import profilePic from "../../public/profile.jpg";
+import githubIcon from "../../public/github.svg";
+import linkedinIcon from "../../public/linkedin.svg";
+import mailIcon from "../../public/mail.svg";
+import home from "../../public/home.svg";
+import info from "../../public/info.svg";
+import project from "../../public/project.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <header className="w-11/12 md:w-8/12 lg:w-5/12 m-auto mt-10 mb-16">
+        <nav className="py-3 flex justify-around items-center rounded-2xl backdrop-blur-xl border border-gray-500">
+          <a className="flex justify-center items-center space-x-3 md:space-x-7">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={home}
+              alt="Home icon"
+              width={24}
+              height={24}
+              className="invert"
             />
-            Deploy now
+            <p>Início</p>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a className="flex justify-center items-center space-x-3 md:space-x-7">
+            <Image src={project} alt="Project icon" width={24} height={24} />
+            <p>Projetos</p>
           </a>
+          <a className="flex justify-center items-center space-x-3 md:space-x-7">
+            <Image
+              src={info}
+              alt="Info icon"
+              width={24}
+              height={24}
+              className="invert"
+            />
+            <p>Sobre mim</p>
+          </a>
+        </nav>
+      </header>
+      <main className="flex justify-center items-center flex-col md:grid md:grid-cols-[1fr_2fr_2fr_1fr] xl:grid-cols-4">
+        <div className="md:flex justify-center items-center space-y-10 md:space-y-0 md:space-x-10 md:col-span-2 md:col-start-2">
+          <Image
+            src={profilePic}
+            alt="Profile picture"
+            width={180}
+            height={180}
+            className="rounded-full m-auto md:m-0 border border-gray-700"
+          />
+          <div>
+            <h1 className="text-center text-5xl md:text-7xl font-extrabold">
+              Arthur Estevão
+            </h1>
+            <div className="flex place-content-around mt-8">
+              <div className="flex items-center rounded-full border py-2 px-6 space-x-2 hover:bg-gray-700 transition ease-out duration-300">
+                <Image
+                  src={githubIcon}
+                  alt="github icon"
+                  width={24}
+                  height={24}
+                  className="invert"
+                />
+                <p className="hidden lg:block">Github</p>
+              </div>
+              <div className="flex items-center rounded-full border py-2 px-6 space-x-2 hover:bg-gray-700 transition ease-out duration-300">
+                <Image
+                  src={linkedinIcon}
+                  alt="linkedIn icon"
+                  width={24}
+                  height={24}
+                />
+                <p className="hidden lg:block">LinkedIn</p>
+              </div>
+              <div className="flex items-center rounded-full border py-2 px-6 space-x-2 hover:bg-gray-700 transition ease-out duration-300">
+                <Image
+                  src={mailIcon}
+                  alt="E-mail icon"
+                  width={24}
+                  height={24}
+                  className="invert"
+                />
+                <p className="hidden lg:block">E-mail</p>
+              </div>
+            </div>
+          </div>
         </div>
+        <section className="w-9/12 md:w-auto m-auto mt-10 row-start-2 col-start-2 col-span-2 max-w-3xl">
+          <p className="text-lg text-center md:text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
